e5edb4853d7d126189376f1706b64132b85e5bf5
Tracey Nduta <traceynduta70@gmail.com>
2023-03-28 11:55:36 +0000

    added docs folder

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,28 @@
+const { description } = require('../../package');
+module.exports = {
+  logo: "https://systechafrica.com/wp-content/uploads/2021/02/outline-fundmaster.png",
+  title: 'FundMaster Documentation',
+  description: description,
+  themeConfig: {
+    logo: 'https://systechafrica.com/wp-content/uploads/2021/02/outline-fundmaster.png',
+    searchPlaceholder: 'Search...',
+    search:true,
+    lastUpdated: 'Last Updated',
+    smoothScroll: true,
+    nextLinks: true,
+    prevLinks: true,
+    nav: [
+      { text: 'Intro', link: '/guide/' },
+      { text: 'Scheme Setup', link: '/schemesetup/' },
+      { text: 'Administration', link: '/administration/' },
+    
+      {
+        text: 'MSS',
+        link: 'https://github.com/KimemiaT/systechafrica.github.io',
+      },
+    ],
+    sidebar: require('./sidebar.js'),
+  },
+};
+  
+
---
+++ docs/src/index.md
@@ -0,0 +1,21 @@
+---
+home: true
+heroImage: https://systechafrica.com/wp-content/uploads/2021/02/outline-fundmaster.png
+tagline: Documentation
+actionText: Get Started →
+actionLink: /guide/
+features:
+- title: MISSION
+  details: Providing innovative, state of the art technology solutions to the retirement benefits industry ensuring industry best practices, compliance and learning, while maintaining motivated human resources.
+- title: VISION
+  details: To be the leading innovative retirement benefits technology solutions provider in Africa.
+- title: QUICK VIDEO TUTORIAL
+  details:  quick video tutorial 
+---
+- [Quick Video Tutorial](https://youtube.com/)
+---
+ ::: slot footer
+ Licensed | Copyright © 2023 [Tracey Nduta](https://www.youtube.com)
+:::
+
+
